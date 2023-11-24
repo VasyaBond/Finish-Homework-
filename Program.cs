e929a73fb360636bfb73e4baa2e51425a3debbae
Jeka <e.g.bondarev@gmail.com>
2023-11-24 16:31:08 +0300

    1 commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // String Array Input
+        Console.WriteLine("Enter strings separated by commas:");
+        string[] originalArray = Console.ReadLine().Split(',');
+
+        // Initializing Iteration Counter
+        int iterationCounter = 0;
+        int newIndex = 0;
